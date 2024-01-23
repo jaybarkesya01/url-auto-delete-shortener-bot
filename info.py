e3edabaae3959946ffb978e812a12d9e04498b55
jaybarkesya01 <157349190+jaybarkesya01@users.noreply.github.com>
2024-01-23 14:35:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -79,10 +79,10 @@ if SELF_DELETE == "True":
 
     # Download Tutorial Button #
 DOWNLOAD_TEXT_NAME = "📥 HOW TO DOWNLOAD 📥"
-DOWNLOAD_TEXT_URL = "https://omegalinks.in/ref/GreyMatter6458"
+DOWNLOAD_TEXT_URL = "https://t.me/+o0foDSwkTz43YzY9"
 
    # Custom Caption Under Button #
-CAPTION_BUTTON = "Subscribe"
-CAPTION_BUTTON_URL = "https://youtube.com/@GreyMattersYT"
+CAPTION_BUTTON = "Join"
+CAPTION_BUTTON_URL = "https://t.me/OTTFilmUpdates"
 
    # Auto Delete For Bot Sending Files #
